9be196a4ea0f8b15d20de960bb40a6e8fbcdd2e6
Francisco Lopez <frankie.b.lopez@gmail.com>
2021-10-16 02:43:06 -0500

    complete to do list

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,26 +1,93 @@
+console.log( 'js' );
+
 $( document ).ready( function(){
-    console.log( 'JQ' );
+  console.log( 'JQ' );
+  $('#taskButton').on('click', createTask); //to add click listener on Create task Button
+  renderTasks()
+});
 
-    $('#taskButton').on('click', createTask);
+
+//Below function will run to create a task on the backend
+
+function createTask() {
+  let taskName = $('#taskInput').val();
+
+  $.ajax({
+    method: "POST",
+    url: "/tasks",
+    data: {
+      name: taskName
+    }
+  }).then(response => {
+
+    renderTasks();
+
+  }).catch(err => {
+
+  })
+}
+
+//Below function will run to get a task from the backend and show the table to the user
+
+function renderTasks() {
+  $.ajax({
+    method: "GET",
+    url: "/tasks"
+  }).then(response => {
+
+    $('#taskList').empty()
 
 
-  }); 
-  
-  function createTask() {
+    response.forEach(element => {
+     let taskClass = ""
+      element.isComplete ? taskClass = "completed-task" : taskClass = "incompleted-task"
+      element.isComplete ? checkOff = "check-off" : checkOff = ""
+        $('#taskList').append(`
+        <li id="${element.id}" class="${taskClass}">
+          <p class="${checkOff}">${element.name}</p>
+          <button id="cmp-${element.id}">Complete</button>
+          <button id="del-${element.id}">Delete</button>
+        </li>
+      `)
+      $(`#cmp-${element.id}`).on('click', onClickCompleteTask);
+      $(`#del-${element.id}`).on('click', onClickDeleteTask);
+      $(`#cmp-${element.id}`).attr('disabled', element.isComplete);
+    });
 
-    let taskName = $('#taskInput').val();
-  
-    $.ajax({
-      method: "POST",
-      url: "/tasks",
-      data: {
-        name: taskName
-      }
-    }).then(response => {
+  }).catch(err => {
+    console.error("GET /tasks failed", err);
+  });
+}
 
-  
-    }).catch(err => {
-  
-    })
-  }
+//Below function will update the user task from incomplete to complete once complete button is pressed for a task
 
+function onClickCompleteTask() {
+  let id = $(this).attr('id').split("-")[1]
+
+  $.ajax({
+    method: "PUT",
+    url: `/tasks/${id}`,
+    data: {
+      isComplete: true,
+    }
+  }).then(response => {
+
+    renderTasks();
+  }).catch(err => {
+
+  });
+}
+
+//Below function will delete a task once delete button is pressed for a task
+
+function onClickDeleteTask() {
+  let id = $(this).attr('id').split("-")[1]
+  $.ajax({
+    method: "DELETE",
+    url: `/tasks/${id}`
+  }).then(response => {
+    renderTasks();
+  }).catch(err => {
+
+  });
+}
--- server/routes/tasks.router.js
+++ server/routes/tasks.router.js
@@ -3,19 +3,29 @@ const express = require('express');
 const tasksRouter = express.Router();
 
 
-
-
 // DB CONNECTION
 
 const pool = require('../modules/pool')
 
 
+tasksRouter.get('/', (req, res) => {
+    let queryString = `SELECT * FROM tasks;`
+    pool.query(queryString)
+      .then(( results ) => {
+        res.send(results.rows);
+      })
+      .catch((err) => {
+        res.sendStatus(500);
+      })
+  });
+
+//create request handler
 
 tasksRouter.post('/', (req, res) => {
     let taskName = req.body.name;
-    
+
     let queryString = `
-      INSERT INTO "tasks" ("name") 
+      INSERT INTO "tasks" ("name")
       VALUES ($1);
     `;
     pool.query(queryString, [taskName])
@@ -23,10 +33,51 @@ tasksRouter.post('/', (req, res) => {
         res.sendStatus(201)
       })
       .catch((err) => {
-          console.log(err);
         res.sendStatus(500);
       });
   });
 
+
+//update request handler
+
+  tasksRouter.put('/:id', (req, res) => {
+
+    let taskId = req.params.id;
+    let isComplete = req.body.isComplete;
+
+
+    let queryString = `
+      UPDATE "tasks"
+      SET "isComplete" = $1
+      WHERE "id" = $2;
+    `;
+    pool.query(queryString, [isComplete, taskId])
+      .then((results) => {
+        res.sendStatus(200)
+      })
+      .catch((err) => {
+        res.sendStatus(500);
+      });
+  });
+
+
+//delete request handler
+
+  tasksRouter.delete('/:id', (req, res) => {
+
+    let taskId = req.params.id;
+
+    let queryString = `
+    DELETE FROM "tasks"
+    WHERE "id" = $1;
+    `;
+    pool.query(queryString, [taskId])
+      .then((results) => {
+        res.sendStatus(200)
+      })
+      .catch((err) => {
+        res.sendStatus(500);
+      });
+  });
 
 module.exports = tasksRouter;
